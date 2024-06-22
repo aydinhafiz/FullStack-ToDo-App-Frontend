293fed248867462c2592a49e25f295dc485ba768
Aydin <112811545+aydinhafiz@users.noreply.github.com>
2024-06-22 09:18:44 +0200

    Update ToDo.jsx

--- src/components/ToDo.jsx
+++ src/components/ToDo.jsx
@@ -6,6 +6,7 @@ import { STodo, STodos } from "../pages/home.styles";
 
 function ToDo({ text, updateMode, deleteTodo }) {
   return (
+    
     <STodos>
       <STodo>
         <div className="text">{text}</div>
